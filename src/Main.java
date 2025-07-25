9af22c0258cb4e188a88c812be8305ca418930dc
Lener Henrique <lenneralves1995@gmail.com>
2025-07-25 09:28:24 -0300

    Dando Vida ao Menu com Lógica e Repetição

--- src/Main.java
+++ src/Main.java
@@ -20,13 +20,39 @@ public class Main {
 
         int opcao = 0;
 
-        System.out.println("1- Consultar saldos");
-        System.out.println("2- Receber valor");
-        System.out.println("3- Transferir valor");
-        System.out.println("4- Sair");
-        System.out.println("Digite a opção desejada:");
-        opcao = scan.nextInt();
+        while (opcao != 4) {
+            System.out.println("1- Consultar saldos");
+            System.out.println("2- Receber valor");
+            System.out.println("3- Transferir valor");
+            System.out.println("4- Sair");
+            System.out.println("Digite a opção desejada:");
+            opcao = scan.nextInt();
 
-        System.out.println("Opção escolhida: " + opcao);
+            if (opcao == 1){
+                System.out.println("O saldo Atual é R$ " +  saldoInicial);
+            }
+            else if (opcao == 2) {
+                System.out.println("Informe o valor a receber: ");
+                double valorRecebido = scan.nextDouble();
+                saldoInicial += valorRecebido;
+                System.out.println("Saldo atualizado R$ " + saldoInicial);
+            }
+            else if (opcao == 3) {
+                System.out.println("Informe o Valor a tranferir:");
+                double valorTransferido = scan.nextDouble();
+                if (valorTransferido > saldoInicial){
+                    System.out.println("Não há saldo suficiente para fazer essa transferência.");
+                }
+                else {
+                    saldoInicial -= valorTransferido;
+                    System.out.println("Saldo atualizado R$ " + saldoInicial);
+                }
+            }
+            else if (opcao != 4) {
+                System.out.println("Opção invalida!");
+            }
+
+        }
+        System.out.println("\nPrograma encerrado. Obrigado por usar nossos serviços!");
     }
 }
